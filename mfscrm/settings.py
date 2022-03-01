8fd0040ab55265e12033ef3ff573361532704e74
Christopher <35177169+steffonic@users.noreply.github.com>
2022-03-01 11:58:55 -0600

    Update settings.py - deploy updates

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -12,10 +12,24 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+#  below was added to deploy
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.template.context_processors import media
+from django.views import static
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# above was added to deploy
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #  BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#  BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) --- this was removed to deploy
+#  Below Was added to deploy
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+#  Above was Added to deploy
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -123,7 +137,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#  below was added to deploy
+if not DEBUG:
+    STATIC_URL = 'static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    MEDIA_ROOT = MEDIA_DIR
+    MEDIA_URL = '/media/'
+else:
+    MEDIA_URL = '/media/'
+    STATIC_URL = '/static/'
+
+#  above was added to deploy
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
